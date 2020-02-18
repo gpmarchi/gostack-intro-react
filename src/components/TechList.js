63f98598876bd22a778b3e78a09f9fb2bc7bce27
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-02-18 17:26:58 -0300

    manipulating state with immutability

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,16 +2,37 @@ import React, { Component } from "react";
 
 class TechList extends Component {
   state = {
-    techs: ["NodeJs", "ReactJs", "React Native"]
+    techs: ["NodeJs", "ReactJs", "React Native"],
+    newTech: ""
+  };
+
+  handleInputChange = event => {
+    this.setState({ newTech: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
   };
 
   render() {
     return (
-      <ul>
-        <li>NodeJS</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          value={this.state.newTech}
+          onChange={this.handleInputChange}
+        />
+        <input type="submit" value="Save" />
+      </form>
     );
   }
 }
